faa6db52214a0f91b6e4d31900070a859a860bbc
phcampp <phcampp@gmail.com>
2025-02-03 01:01:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para abrir o pop-up
+function openPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+// Função para fechar o pop-up
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
